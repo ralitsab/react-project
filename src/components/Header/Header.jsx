ee9f1bdc3ad6d571d1fa79b62b79b3dd2ecb695f
Ralitsa Borisova <rborisova@iranigroup.co.il>
2024-08-04 20:51:20 +0300

    fix logout modal styling, add error handling, move logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,101 +9,88 @@ import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useAuthProvider } from "../../context/authProvider";
-import { logoutUser } from "../../services/authService";
-import { useNavigate } from "react-router-dom";
 import LogoutModal from "./logout/LogoutModal";
 import SearchForm from "./search/SearchForn";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const { currentUser, logout } = useAuthProvider();
+  const { currentUser } = useAuthProvider();
   const [isOpen, setOpen] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
   };
+
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handleLogout = async (e) => {
-    e.preventDefault();
-    try {
-      logoutUser();
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
-
   return (
-    <header className={`${styles.header} flex justify-between items-center p-4 shadow-md pl-20 pr-20`}>
-    <div className="flex flex-col md:flex-row justify-between items-center w-full">
-      <div className="flex space-x-6 mb-4 md:mb-0 flex-1 items-center">
-        <Link
-          to="/products/grape-1"
-          className={`${styles.nav_link} p-10 border-solid bg-[#14433D] rounded-full border-2 font-bold text-lg text-center text-white pt-2 pb-2 transition ease-in-out delay-150 hover:bg-[#14433D] hover:text-white duration-300`}
-        >
-          Shop
-        </Link>
-        <a
-          href="#learn"
-          className={`${styles.nav_link} hover:text-gray-900 text-[#14433D]`}
-        >
-          Learn
-        </a>
-        <a
-          href="#subscribe"
-          className={`${styles.nav_link} hover:text-gray-900 text-[#14433D]`}
-        >
-          Subscribe
-        </a>
-      </div>
-      <Link to="/" className="flex justify-center items-center flex-1">
-        <img src="../public/brand/logo.webp" alt="Logo" className="h-12" />
-      </Link>
-      <div className="flex space-x-6 flex-1 justify-end items-center">
-        <div className="relative text-gray-600">
-          <SearchForm />
+    <header
+      className={`${styles.header} flex justify-between items-center p-4 shadow-md pl-20 pr-20`}
+    >
+      <div className="flex flex-col md:flex-row justify-between items-center w-full">
+        <div className="flex space-x-6 mb-4 md:mb-0 flex-1 items-center">
+          <Link
+            to="/products/grape-1"
+            className={`${styles.nav_link} p-10 border-solid bg-mainGreen rounded-full border-2 font-bold text-lg text-center text-white pt-2 pb-2 transition ease-in-out delay-150 hover:bg-mainGreen hover:text-white duration-300`}
+          >
+            Shop
+          </Link>
+          <a
+            href="#learn"
+            className={`${styles.nav_link} hover:text-gray-900 text-mainGreen`}
+          >
+            Learn
+          </a>
+          <a
+            href="#subscribe"
+            className={`${styles.nav_link} hover:text-gray-900 text-mainGreen`}
+          >
+            Subscribe
+          </a>
         </div>
-  
-        <Link to="/account">
-          <FontAwesomeIcon
-            icon={faUser}
-            className="mr-2 text-[#14433D]"
-            style={{ height: "20px", width: "20px" }}
-          />
+        <Link to="/" className="flex justify-center items-center flex-1">
+          <img src="../public/brand/logo.webp" alt="Logo" className="h-12" />
         </Link>
-  
-        {currentUser && (
-          <>
-            <button onClick={handleOpen} className="flex items-center">
-              <FontAwesomeIcon
-                icon={faSignOutAlt}
-                className="mr-2 text-[#14433D]"
-                style={{ height: "20px", width: "20px" }}
-              />
-            </button>
-            <LogoutModal
-              isOpen={isOpen}
-              onClose={handleClose}
-              handleLogout={handleLogout}
+        <div className="flex space-x-6 flex-1 justify-end items-center">
+          <div className="relative text-gray-600">
+            <SearchForm />
+          </div>
+
+          <Link to="/account">
+            <FontAwesomeIcon
+              icon={faUser}
+              className="mr-2 text-mainGreen"
+              style={{ height: "20px", width: "20px" }}
             />
-          </>
-        )}
-  
-        <a href="/cart" className="text-gray-700 font-semibold flex items-center">
-          <FontAwesomeIcon
-            icon={faBasketShopping}
-            className="mr-2 text-[#14433D]"
-            style={{ height: "20px", width: "20px" }}
-          />
-        </a>
+          </Link>
+
+          {currentUser && (
+            <>
+              <button onClick={handleOpen} className="flex items-center">
+                <FontAwesomeIcon
+                  icon={faSignOutAlt}
+                  className="mr-2 text-mainGreen"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </button>
+            </>
+          )}
+
+          <LogoutModal isOpen={isOpen} onClose={handleClose} />
+
+          <a
+            href="/cart"
+            className="text-gray-700 font-semibold flex items-center"
+          >
+            <FontAwesomeIcon
+              icon={faBasketShopping}
+              className="mr-2 text-mainGreen"
+              style={{ height: "20px", width: "20px" }}
+            />
+          </a>
+        </div>
       </div>
-    </div>
-  </header>
-  
+    </header>
   );
 }
--- src/components/Header/Logout/LogoutModal.jsx
+++ src/components/Header/Logout/LogoutModal.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
 import styles from './logoutModal.module.css';
+import { logoutUser } from '../../../services/authService';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-const LogoutModal = ({ isOpen, onClose, handleLogout }) => {
+const LogoutModal = ({ isOpen, onClose }) => {
+
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
   if (!isOpen) return null;
+  
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    try {
+      logoutUser();
+      onClose();
+      navigate("/");
+    } catch (error) {
+      setError('Logout failed. Please try again.');
+    }
+  };
+
 
   return (
-    <div className={`modal_overlay ${styles.modal_overlay}`}>
-      <div className={`modal_content ${styles.modal_content}`}>
+    <div className={styles.modal_overlay}>
+      <div className={styles.modal_content}>
         <button
-          className={`modal_close absolute top-2 right-2 text-gray-500 hover:text-gray-700 ${styles.modal_close}`}
+          className={styles.modal_close}
           onClick={onClose}
         >
           &times;
         </button>
         <div className="content p-4 flex flex-col">
-          <h2 className="m-0 p-0 font-display text-5xl md:text-2xl text-[#14433D] text-center font-bold pb-10">Are you sure you want to logout?</h2>
+          <h2 className="m-0 p-0 font-display text-xl md:text-2xl text-mainGreen text-center font-bold pb-10">
+            Are you sure you want to logout?
+          </h2>
           <div className="flex justify-center space-x-4">
             <button
-              className="submit-button border-solid rounded-full font-bold text-lg text-center bg-[#14433D] text-white py-2 px-10 mb-8 transition ease-in-out delay-150 hover:bg-[#12332A] hover:text-white duration-300"
+              className="submit-button border-solid rounded-full font-bold text-lg text-center bg-mainGreen text-white py-2 px-10 mb-8 transition ease-in-out delay-150 hover:bg-[#12332A] hover:text-white duration-300"
               onClick={handleLogout}
             >
               Logout
             </button>
             <button
-              className="submit-button border-solid rounded-full font-bold text-lg text-center bg-[#14433D] text-white py-2 px-10 mb-8 transition ease-in-out delay-150 hover:bg-[#12332A] hover:text-white duration-300"
+              className="submit-button border-solid rounded-full font-bold text-lg text-center bg-mainGreen text-white py-2 px-10 mb-8 transition ease-in-out delay-150 hover:bg-[#12332A] hover:text-white duration-300"
               onClick={onClose}
             >
               Cancel
             </button>
           </div>
         </div>
+        {error && <div className="text-red-500 mt-2">{error}</div>}
+
       </div>
     </div>
   );
