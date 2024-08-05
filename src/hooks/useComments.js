7df6d08de362b6e9ce5c5c8fefb18046b7f889be
Ralitsa Borisova <rborisova@iranigroup.co.il>
2024-08-05 23:37:30 +0300

    change hook extensions

---
+++ src/hooks/useComments.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { addComment, getComments, deleteUserComment } from "../services/commentService";
+import { useAuthProvider } from "../context/authProvider"
+
+
+export const useComments = (initialProductId) => {
+  const [productId, setProductId] = useState(initialProductId);
+  const [error, setError] = useState(null); 
+  const [comments, setComments] = useState([]);
+  const { currentUser, userProfile } = useAuthProvider();
+
+
+  const fetchComments = async () => {
+    setError(null);
+    try {
+      const comments = await getComments(productId);
+      setComments(comments);
+    } catch (error) {
+      setError('Failed to fetch comments. Please try again later.');
+    }
+  };
+  
+  useEffect(() => {
+    fetchComments();
+  }, [productId]);
+
+  useEffect(() => {
+    setProductId(initialProductId);
+  }, [initialProductId]);
+
+
+  const addNewComment = async (heading, comment) => {
+    try {
+     await addComment(
+      productId,
+      currentUser.uid,
+      userProfile.firstName,
+      userProfile.surname,
+      comment,
+      heading
+    );
+    fetchComments();
+  }catch {
+    setError('Failed to add comments. Please try again later.');
+  }
+  };
+
+  const deleteComment = async (commentId) => {
+    try {
+      await deleteUserComment(productId, commentId, currentUser.uid);
+      fetchComments();
+    } catch (error) {
+      setError('Failed to delete comment. Please try again later.');
+    }
+  };
+  return {
+    comments,
+    deleteComment,
+    fetchComments,
+    addNewComment,
+    error
+  };
+};
---
+++ src/hooks/useSearch.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { useProducts } from "./useProducts";
+
+export const useSearch = (searchTerm) => {
+  const { products, loading, error } = useProducts();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    if (!loading && products.length > 0) {
+      const lowercasedSearchTerm = searchTerm.toLowerCase();
+      const filtered = products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(lowercasedSearchTerm) ||
+          product.id.toLowerCase().includes(lowercasedSearchTerm)
+      );
+      setFilteredProducts(filtered);
+    }
+  }, [searchTerm, products, loading]);
+
+  return { filteredProducts, loading, error };
+};
+
